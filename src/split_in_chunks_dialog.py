f943f72a83711644bb6591924ee5de96be8146c1
ErosSafh <58816138+ErosSafh@users.noreply.github.com>
2019-12-12 11:35:26 -0300

    Update split_in_chunks_dialog.py

--- src/split_in_chunks_dialog.py
+++ src/split_in_chunks_dialog.py
@@ -361,11 +361,10 @@ class SplitDlg(QtWidgets.QDialog):
         try:
             classify
         except NameError :
-        else
-            if classify:
-                for i in range(len(Metashape.app.document.chunks)):
-                    if i + 1 < (len(Metashape.app.document.chunks)):
-                        Metashape.app.document.chunks[i + 1].dense_cloud.classifyPoints(confidence = 0.0)
+        elif classify:
+            for i in range(len(Metashape.app.document.chunks)):
+                if i + 1 < (len(Metashape.app.document.chunks)):
+                    Metashape.app.document.chunks[i + 1].dense_cloud.classifyPoints(confidence = 0.0)
         # Fim Mod classify
         # Save .laz
         for i in range(len(Metashape.app.document.chunks)):
